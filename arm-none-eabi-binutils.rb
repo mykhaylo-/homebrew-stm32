066b788bad04f517addc8f2dfdf86ce02bc3d4c8
mykhaylo- <michael.lviv@gmail.com>
2016-02-08 07:54:54 +0200

    Updated binutils version to 2.26

--- arm-none-eabi-binutils.rb
+++ arm-none-eabi-binutils.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class ArmNoneEabiBinutils < Formula
   homepage 'http://www.gnu.org/software/binutils/binutils.html'
-  url 'http://ftpmirror.gnu.org/binutils/binutils-2.24.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/binutils/binutils-2.24.tar.gz'
-  sha1 '1b2bc33003f4997d38fadaa276c1f0321329ec56'
+  url 'http://ftpmirror.gnu.org/binutils/binutils-2.26.tar.gz'
+  mirror 'http://ftp.gnu.org/gnu/binutils/binutils-2.26.tar.gz'
+  sha1 'a637508cffeb2323b14bc8dd65378819768ad4ff'
 
   def install
     args = ["--disable-debug",
